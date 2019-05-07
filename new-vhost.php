3eaccee3e7a5f5cf75ecc20ca209dd25c337ed56
Tadhg <tadhgsmboyle@gmail.com>
2019-05-06 19:12:12 -0700

    not currently working
    if ssl is selected, the certificate is created but either the virtualhosts are not enabled correctly or something. will fix soon

--- new-vhost.php
+++ new-vhost.php
@@ -7,10 +7,16 @@ include 'config.php';
 
 //variables from form
 $domain = $_POST['domain'];
-    
+$ssl = $_POST['ssl'];
+
 //create www-data folder
 if (!file_exists("$wwwData/$domain")) {
-    mkdir("$wwwData/$domain", 0777, true);
+mkdir("$wwwData/$domain", 0777, true);
+    
+//add dummy index.html
+$fp = fopen("$wwwData/$domain/" . "index" . '.html', "w+");
+fwrite($fp,"It works!");
+fclose($fp);
 }
 
 //create + write to vhost file
@@ -27,5 +33,30 @@ fwrite($fp,
 </VirtualHost>");
 
 fclose($fp);
+
+//did they say yes to ssl :)
+if($ssl == "yes")
+{
+echo "Running Certbot...<br>";
+exec ("sudo certbot --apache --non-interactive --agree-tos --redirect --register-unsafely-without-email -d $domain -d www.$domain 2>&1", $output, $return_var);
     
+var_dump($return_var);
+echo "return_var is: $return_var" . "\n";
+var_dump($output);
+
+echo "Reloading Apache...<br>";
+exec ("sudo apache2ctl graceful");
+echo "<a href=https://$domain>Click here to go to your site!</a>";
+}
+
+//or did they say no :(
+else if($ssl == "no")
+{
+echo "Enabling VirtualHost...<br>";
+exec ("sudo a2ensite $domain");
+echo "Reloading Apache...<br>";
+exec ("sudo apache2ctl graceful"); 
+echo "<a href=http://$domain>Click here to go to your site!</a>";
+}
+
 ?>
